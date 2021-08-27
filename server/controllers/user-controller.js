50ddc715ca083c9e7e862cc510d1f809c2202600
Brett Fleming <brettfleming015@gmail.com>
2021-08-27 11:53:20 -0500

    added files to server folder, getting server up and running

---
+++ server/controllers/user-controller.js
@@ -0,0 +1,44 @@
+const { User, Post, Colony } = require('../models');
+// import sign token function from auth
+const { signToken } = require('../utils/auth');
+
+
+module.exports = {
+    // async getSingleUser({ user = null, params }, res) {
+    //     const foundUser = await User.findOne({
+    //       $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
+    //     });
+    
+    //     if (!foundUser) {
+    //       return res.status(400).json({ message: 'Cannot find user' });
+    //     }
+    
+    //     res.json(foundUser);
+    // },
+
+    async createUser({ body }, res) {
+        // console.log(body);
+        const user = await User.create(body);
+        // console.log(user)
+        if (!user) {
+          return res.status(400).json({ message: 'User not created' });
+        }
+        const token = signToken(user);
+        res.json({ token, user });
+    },
+
+    async login({ body }, res) {
+        const user = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] });
+        if (!user) {
+          return res.status(400).json({ message: "Invalid credentials" });
+        }
+    
+        const correctPw = await user.isCorrectPassword(body.password);
+    
+        if (!correctPw) {
+          return res.status(400).json({ message: 'Invalid credentials' });
+        }
+        const token = signToken(user);
+        res.json({ token, user });
+    },
+}
