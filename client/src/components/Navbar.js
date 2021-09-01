ed61f1f7a3ba0ee8b3141425e27a540b9c79c56a
Brett Fleming <brettfleming015@gmail.com>
2021-09-01 11:21:46 -0500

    started Navbar

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'reqact-router-dom';
+import { Navbar, Nav, Container, Tab } from 'react-bootstrap';
+
+
+const AppNavbar = () => {
+
+    return (
+        <>
+        <Navbar expand='lg' className='t-nav-bar'>
+            <Container fluid>
+                
+            </Container>
+        </Navbar>
+        </>
+    )
+}
